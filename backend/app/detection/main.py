044cbfbbd62a5055b9fd9778bdedee5c60c99490
maxmax <maxbakaiev@gmail.com>
2023-12-30 14:05:58 +0200

    car count

--- backend/app/detection/main.py
+++ backend/app/detection/main.py
@@ -1,5 +1,42 @@
+import os
 import cv2
+import time
 
-def detectionService(data):
 
-    return data
+class DetectionTraffic:
+    def __init__(self, video_path):
+        self.video_path = video_path
+        self.detector = cv2.HOGDescriptor()
+        self.detector.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    def count_cars(self):
+        cap = cv2.VideoCapture(self.video_path)
+
+        start_time = time.time()
+        end_time = start_time + 10  # Time in seconds
+
+        car_count = 0
+
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            # Convert the frame to gray
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Use an object detector
+            boxes, weights = self.detector.detectMultiScale(gray, winStride=(8, 8))
+
+            # Draw rectangles around detected objects
+            for (x, y, w, h) in boxes:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                car_count += 1
+
+            if time.time() > end_time:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+        return car_count
--- backend/app/main/api.py
+++ backend/app/main/api.py
@@ -3,11 +3,19 @@ from flask import jsonify, make_response, request, render_template
 from app import db
 from app.main import bp
 from app.analysis import main as analysis
+from app.detection import main as detection
 from app.models import User, Resource
 
 @bp.route('/api-info', methods=["GET"])
-def api_info():
+def apiInfo():
     return jsonify('Api info!')
+
+@bp.route('/api-traffic', methods=["GET"])
+def apiDetectionTraffic():
+    video_path = 'https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4'
+    detection_traffic = detection.DetectionTraffic(video_path)
+    car_count = detection_traffic.count_cars()
+    return jsonify({'car_count': car_count})
 
 @bp.errorhandler(404)
 def not_found(error):
